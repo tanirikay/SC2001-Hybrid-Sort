6c6bf76ed6b16ce92da1e6a70182129069de2c51
Jmin12 <thangjingmin@gmail.com>
2026-09-18 00:21:46 +0800

    Added part C(ii)

--- experiment.py
+++ experiment.py
@@ -1,39 +1,21 @@
 import matplotlib.pyplot as plt
-import math
 from hybrid_sort import generate_random_array, run_hybrid_sort, run_merge_sort
-#Import hybrid sort and random array here
-#get_dataset(n) --> random array from (b)
-#hybrid_sort(arr, s) --> sort algorithm from (a)
-n_values = [1000,2000,5000,10000,20000,50000,100000,200000,500000,1000000,2000000,5000000,10000000]
-S = 20
-comparison_results = []
-theoretical_results = []
-ratios = []
 
 #----------------------
 #   Experiment c(i)
 #----------------------
+n_values = [1000,2000,5000,10000,20000,50000,100000,200000,500000,1000000,2000000,5000000,10000000]
+S = 20
+comparison_results = []
 
+print("----------------------")
+print("   Experiment c(i)")
+print("----------------------")
 for n in n_values:
     arr = generate_random_array(n)
     comparison = run_hybrid_sort(arr, S)
     comparison_results.append(comparison)
     print("n =", n, "comparison =", comparison)
-
-#Theoretical analysis of time complexity (n log n)
-for n in n_values:
-    #calculate n log2 n, log2 is used because merge sort split into 2 halves
-    theoretical = n * math.log2(n)
-    theoretical_results.append(theoretical)
-
-#Comparing empirical results with theoretical analysis
-for i in range(len(n_values)):
-    #if ratio is about '1', it shows that time complexity is (n log n)
-    ratio = comparison_results[i]/theoretical_results[i]
-    ratios.append(ratio)
-    print("n =",n_values[i] , f"ratio = {ratio:.2f}")
-
-
 
 plt.plot(n_values,comparison_results,marker = "o",label = "Hybrid Sort")
 plt.xscale("log")
@@ -46,8 +28,34 @@ plt.grid()
 plt.savefig("part_c_i_.png")
 plt.show()
 
-#Part C(ii)
+#----------------------
+#   Experiment c(ii)
+#----------------------
+n_value_ii = 100000
+s_value = [1,2,5,10,20,50,100,200,500,1000]
 
-#Part C(iii)
+comparison_results_ii = []
+
+arr_ii = generate_random_array(n_value_ii)
+print("----------------------")
+print("   Experiment c(ii)")
+print("----------------------")
+for s_ii in s_value:
+    comparsion_ii = run_hybrid_sort(arr_ii, s_ii)
+    comparison_results_ii.append(comparsion_ii)
+    print("S =", s_ii, "comparison =", comparsion_ii)
+
+plt.plot(s_value, comparison_results_ii, marker="o")
+
+plt.xlabel("Threshold S")
+plt.ylabel("Number of Key Comparisons")
+plt.title(f"Key Comparisons vs Threshold S (n = {n_value_ii})")
+
+plt.grid()
+plt.savefig("part_c_ii.png")
+plt.show()
+#----------------------
+#   Experiment c(iii)
+#----------------------
 
 #Part D
